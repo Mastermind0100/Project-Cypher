61314c0b775343609fd11cc7d05d25de1fb8d80b
Atharva <atharvahudlikar@gmail.com>
2021-04-01 13:44:55 +0530

    Client Side code added

---
+++ app/src/main/java/com/example/projectcypher/SensorScreen.java
@@ -0,0 +1,51 @@
+package com.example.projectcypher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SensorScreen extends AppCompatActivity {
+
+    TextView textView;
+    Button connectbutton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_screen);
+        getSupportActionBar().hide();
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        textView = (TextView) findViewById(R.id.servertext);
+        connectbutton = (Button) findViewById(R.id.connectionbutton);
+
+        connectbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Socket s = new Socket("192.168.1.30", 5000);
+                    DataInputStream inputStream = new DataInputStream(s.getInputStream());
+                    String string = (String)inputStream.readUTF();
+                    textView.setText(string);
+                    textView.invalidate();
+                    textView.requestLayout();
+                    s.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
